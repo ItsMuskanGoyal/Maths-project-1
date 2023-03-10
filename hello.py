426fb5d49c401e4144a63897d96d67d7ab2a63c8
Muskan Goyal <itsmuskangoyal07@gmail.com>
2023-03-10 16:49:45 +0530

    rect

--- hello.py
+++ hello.py
@@ -10,3 +10,7 @@ print("Area of circle ", r, "is :", 3.14*r*r)
 h= int(input("Enter Height of Parallelogram"))
 w= int(input("Enter length of base of parallelogram"))
 print("Area of parallelogram with height ",h, "and Base ",w,"is : ", w*h*(0.5))
+
+h1= int(print("Enter Height of a rectangle"))
+b1= int(input("Enter base "))
+print("Area is: " ,h1*b1)
