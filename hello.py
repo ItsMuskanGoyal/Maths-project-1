3de5df2f86d8c54854d08821fa5dde08f9b61bcd
Muskan Goyal <itsmuskangoyal07@gmail.com>
2023-03-10 14:11:56 +0530

    Added area of pp and circle

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ a= int(input('Enter Base number'))
 b= int(input("Enter Power"))
 res= a**b
 print(a,"raise to power",b,"is : ", res)
+
+r= int(input("Enter radius of circle"))
+print("Area of circle ", r, "is :", 3.14*r*r)
+
+h= int(input("Enter Height of Parallelogram"))
+w= int(input("Enter length of base of parallelogram"))
+print("Area of parallelogram with height ",h, "and Base ",w,"is : ", w*h*(0.5))
