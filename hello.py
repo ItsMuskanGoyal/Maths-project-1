ad79289eac805ba0e49080c51ed797b1c5c47bc8
Muskan Goyal <itsmuskangoyal07@gmail.com>
2023-03-10 14:00:53 +0530

    underroot feature added by branching

--- hello.py
+++ hello.py
@@ -3,3 +3,6 @@ a= int(input('Enter Base number'))
 b= int(input("Enter Power"))
 res= a**b
 print(a,"raise to power",b,"is : ", res)
+
+c= int(input(print ("Enter number to get Underroot")))
+print("under value of ",c,"is : ", c**(0.5))
