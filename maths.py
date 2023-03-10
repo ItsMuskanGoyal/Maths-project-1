c4db054e05b5fb8a94d90437187f7a40b7c0e26e
Muskan Goyal <itsmuskangoyal07@gmail.com>
2023-03-10 16:53:46 +0530

    final+circle

---
+++ maths.py
@@ -0,0 +1,20 @@
+print ("Hello World. Let's calculate Power of a number")
+a= int(input('Enter Base number'))
+b= int(input("Enter Power"))
+res= a**b
+print(a,"raise to power",b,"is : ", res)
+
+c= int(input(print ("Enter number to get Underroot")))
+print("under value of ",c,"is : ", c**(0.5))
+
+
+h= int(input("Enter Height of Parallelogram"))
+w= int(input("Enter length of base of parallelogram"))
+print("Area of parallelogram with height ",h, "and Base ",w,"is : ", w*h*(0.5))
+
+h1= int(print("Enter Height of a rectangle"))
+b1= int(input("Enter base "))
+print("Area is: " ,h1*b1)
+
+r= int(print('Enter radius for area: '))
+print("Area of circle is :", (22/7)*r*r)
